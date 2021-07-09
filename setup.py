06e1651205a5b4bd9bc055d9b1d6ebe7dc5c5647
mb-wali <mb_wali@hotmail.com>
2021-07-09 11:58:59 +0200

    dep

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ history = open('CHANGES.rst').read()
 tests_require = [
     'invenio-accounts[sqlite]>=1.3.0',
     'invenio-assets>=1.2.0',
-    'invenio-db>=1.0.8',
-    'pytest-invenio>=1.4.0',
+    'invenio-db>=1.0.9',
+    'pytest-invenio>=1.4.2',
 ]
 
 extras_require = {
